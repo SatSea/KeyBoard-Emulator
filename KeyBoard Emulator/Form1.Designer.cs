6cf1baf0a7abb43306ebe55ac439c1719e094edc
SatSea <staspetrov388@gmail.com>
2023-03-28 19:22:47 +0300

    Добавьте файлы проекта.

---
+++ KeyBoard Emulator/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace KeyBoard_Emulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loadTextFromFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button1.Location = new System.Drawing.Point(89, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Активировать хоткей";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EmulateButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(307, 143);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текст из файла";
+            // 
+            // loadTextFromFile
+            // 
+            this.loadTextFromFile.Location = new System.Drawing.Point(15, 41);
+            this.loadTextFromFile.Name = "loadTextFromFile";
+            this.loadTextFromFile.Size = new System.Drawing.Size(93, 23);
+            this.loadTextFromFile.TabIndex = 5;
+            this.loadTextFromFile.Text = "Выбор файла";
+            this.loadTextFromFile.UseVisualStyleBackColor = true;
+            this.loadTextFromFile.Click += new System.EventHandler(this.LoadTextFromFile);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Выбор файла с текстом";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(9, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Эмуляция: не запущена";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Скорость печати";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "медленно",
+            "средне",
+            "быстро",
+            "очень быстро (опасно)"});
+            this.comboBox1.Location = new System.Drawing.Point(198, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 341);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loadTextFromFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loadTextFromFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ KeyBoard Emulator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using NHotkey;
+using NHotkey.WindowsForms;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyBoard_Emulator
+{
+    public partial class Form1 : Form
+    {
+        private int minTimeType;
+        private int maxTimeType;
+
+        private string TextFromTextFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 1;
+            MessageBox.Show("Хоткеи:\nctrl+alt+r - начать эмуляцию набора текста;\nctrl+alt+d - экстренное завершение программы", "Хоткеи");
+        }
+
+        private void EmulateButton_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Нет текста для эмуляции", "Нет текста");
+                return;
+            }
+            HotkeyManager.Current.AddOrReplace("Start Emulation", Keys.Control | Keys.Alt | Keys.R, OnHotkeyEmulation);
+            HotkeyManager.Current.AddOrReplace("Stop Emulation", Keys.Control | Keys.Alt | Keys.D, emergency_exit);
+            label1.Text = "Эмуляция: хоткеи заармированы";
+        }
+
+        private void OnHotkeyEmulation(object sender, HotkeyEventArgs e)
+        {
+            HotkeyManager.Current.Remove("Start Emulation");
+            e.Handled = true;
+            StartEmulation();
+        }
+
+        private void StartEmulation()
+        {
+            label1.Text = "Эмуляция: запущена";
+            Thread.Sleep(150);
+            Emulation();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            HotkeyManager.Current.Remove("Start Emulation");
+            HotkeyManager.Current.Remove("Stop Emulation");
+            MessageBox.Show("Change Da World… My Final Message. Goodbye");
+        }
+
+        private void emergency_exit(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Emulation()
+        {
+            try
+            {
+                Random random = new Random();
+                foreach (char letter in TextFromTextFile)
+                {
+                    SendKeys.SendWait(letter.ToString());
+                    Thread.Sleep(random.Next(minTimeType, maxTimeType));
+                }
+            }
+            catch
+            {
+                label1.Text = "Эмуляция: завершена с ошибкой";
+                MessageBox.Show("Прозошла ошибка и текст был проэмулирован неполностью или не проэмулирован вовсе.", "Ошибка");
+                Application.Exit();
+            }
+            finally
+            {
+                label1.Text = "Эмуляция: завершена";
+                MessageBox.Show("Текст проэмулирован.", "Эмуляция завершена");
+            }
+        }
+
+        private void LoadTextFromFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "TXT files|*.txt";
+            if (openFileDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                MessageBox.Show("Не смог получить файл, попробуй еще раз.", "Чет пошло не так");
+                return;
+            }
+            TextFromTextFile = File.ReadAllText(openFileDialog.FileName);
+            MessageBox.Show("Файл загружен и программа готова к эмуляции.", "Файл загружен");
+            richTextBox1.Text = TextFromTextFile;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    minTimeType = 31;
+                    maxTimeType = 38;
+                    break;
+
+                case 1:
+                    minTimeType = 19;
+                    maxTimeType = 24;
+                    break;
+
+                case 2:
+                    minTimeType = 10;
+                    maxTimeType = 14;
+                    break;
+
+                case 3:
+                    minTimeType = 5;
+                    maxTimeType = 8;
+                    break;
+            }
+        }
+    }
+}
